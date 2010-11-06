e3c1f51395f69b59c2314b615059598353b1fd7a
Robert Diamond <rmd6502@gmail.com>
2010-11-06 12:09:46 -0400

    initial commit

---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPad.h
+//  A1Lights
+//
+//  Created by Robert Diamond on 11/6/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ iPad/MainLightViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainLightViewController.h
+//  A1Lights
+//
+//  Created by Robert Diamond on 11/6/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainLightViewController : UIViewController {
+
+}
+
+@end
